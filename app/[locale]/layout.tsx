c658378d4a0fecfda0c8b29413cd76abfa90d55b
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-12-20 19:25:40 +0200

    Update Open Graph image path and add favicon asset for improved asset management

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -63,7 +63,7 @@ export default async function RootLayout({
         <meta property="og:locale:alternate" content="en" />
         <meta
           property="og:image"
-          content="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          content="https://drive.google.com/file/d/1mHm_0oaE7Cn3iSpa8OFn_TDtCl1lYY--/view?usp=sharing"
         />
         <meta property="og:type" content="website" />
       </head>
